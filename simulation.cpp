df4fad08dd13e29e36a5cb4be6afa1f8dd0feccb
Phillip Vea <phillipvea2408@gmail.com>
2018-04-18 15:19:05 -0400

    updating

--- simulation.cpp
+++ simulation.cpp
@@ -1,7 +1,7 @@
 #include "simulation.h"
 
 
-Coordinate::Coordinate() {//is this automatically called when coordinate object is declared?
+Coordinate::Coordinate() {
     init(0, 0, 0);
 }
 
@@ -13,8 +13,8 @@ Coordinate::Coordinate(double x, double y, double theta) {
     init(x, y, theta);
 }
 
-void Coordinate::init(double x, double y, double theta) {//when is this method called? Seems to be called when the coordinate objects are declared in the header file
-    this->x = x;    //why does this use arrow instead of . notation? Coordinate is not a pointer???
+void Coordinate::init(double x, double y, double theta) {
+    this->x = x; 
     this->y = y;
     this->theta = theta;
 }
@@ -30,9 +30,7 @@ DFRTankSimulation::DFRTankSimulation(String name) {
   Serial.print(name);
 };
 
-void DFRTankSimulation::init(){ //Should this code be combined with constructor?
-  //should this create a thread that updates coordinates with using messages from simulation?
-
+void DFRTankSimulation::init(){ 
   //think about moving this line to the constructor and removing the method
   Serial.begin(9600);
 };
@@ -41,7 +39,7 @@ void DFRTankSimulation::setLeftMotorPWM(int pwm) {
   
   Serial.print("#l");
   Serial.print(pwm);
-  Serial.println("*");    //do any of these print statements to the console need to have new line endings?
+  Serial.println("*");
   Serial.flush();
 
   _left_motor_pwm = pwm;
@@ -115,11 +113,8 @@ bool DFRTankSimulation::updateLocation() {
     
     while((millis() - start) < 600) {
         if(Serial.available()) {
-            switch(state) { //where does the '#' and '*' go when reading input? Does the software take care of that? Is it not sent at all?
-                            //it is not sent at all- don't need those characters if nothing is interfering with the serial port
-                            //probably don't need it for this, but should include just in case students leave println's in their code, and will also 
-                            //help for future support of printing.
-                case 0:     //when writing communication, need to modify on other end to take out mId because it is not needed for the simulation
+            switch(state) {
+                case 0:
                     location.x = Serial.parseFloat();
                     state++;
                     break;
@@ -137,7 +132,6 @@ bool DFRTankSimulation::updateLocation() {
             }
         }
     }
-    //add Serial.flush() to end of function?
     return false;
 }
 
